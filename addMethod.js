dc0c2efee49d6c3d44497d4be60b2786cd9f8d2d
istavros <istavros@gmail.com>
2014-09-11 22:51:16 +0300

    create addMethod.js

---
+++ addMethod.js
@@ -0,0 +1,27 @@
+// addMethod - By Stavros Ioannidis (MIT Licensed)
+function addMethod(obj, name, fn) {
+	obj[name] = obj[name] || function() {
+  
+		// get the cached method with arguments.length arguments
+		var method = obj[name].cache[arguments.length];
+ 
+		// if method exists call it 
+		if ( !! method)
+			return method.apply(this, arguments);
+			else throw new Error("Wrong number of arguments");
+	};
+ 
+	// initialize obj[name].cache
+	obj[name].cache = obj[name].cache || {};
+ 
+ 
+	// Check if a method with the same 
+	// number of arguments exists  
+	if ( !! obj[name].cache[fn.length])
+		throw new Error("Cannot define multiple '" + name + "' methods with the same number of arguments!");
+ 
+	// cache the method with fn.length arguments
+	obj[name].cache[fn.length] = function() {
+		return fn.apply(this, arguments);
+	};
+}
