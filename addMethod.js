f416e617b0b8e0cf2d3b27611790f6b6e2459f16
Stavros Ioannidis <istavros@gmail.com>
2014-10-14 19:47:18 +0300

    remove unnecessary cache object

--- addMethod.js
+++ addMethod.js
@@ -1,28 +1,24 @@
 // addMethod - By Stavros Ioannidis (MIT Licensed)
 function addMethod(obj, name, fn) {
 	obj[name] = obj[name] || function() {
-  
+
 		// get the cached method with arguments.length arguments
-		var method = obj[name].cache[arguments.length];
- 
+		var method = obj[name][arguments.length];
+
 		// if method exists call it 
-		if ( !! method)
+		if (!!method)
 			return method.apply(this, arguments);
 		else
 			throw new Error("Wrong number of arguments");
 	};
- 
-	// initialize obj[name].cache
-	obj[name].cache = obj[name].cache || {};
- 
- 
+
 	// Check if a method with the same 
 	// number of arguments exists  
-	if ( !! obj[name].cache[fn.length])
+	if (!!obj[name][fn.length])
 		throw new Error("Cannot define multiple '" + name + "' methods with the same number of arguments!");
- 
+
 	// cache the method with fn.length arguments
-	obj[name].cache[fn.length] = function() {
+	obj[name][fn.length] = function() {
 		return fn.apply(this, arguments);
 	};
 }
